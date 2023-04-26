82811829528ee9b0fce2e7d8cfea835fb5177774
Kurller <kolaquadry@gmail.com>
2023-04-26 06:08:46 -0700

    task done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    # Initialize a list to keep track of visited boxes
-    visited = [False] * len(boxes)
-    visited[0] = True  # Mark the first box as visited
-
-    # Initialize a list to keep track of keys we have
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
     keys = boxes[0]
-
-    # Iterate through the keys list
-    while keys:
-        key = keys.pop(0)  # Get the first key from the list
-        if not visited[key]:
-            visited[key] = True  # Mark the box as visited
-            keys += boxes[key]  # Add the keys from the box to the keys list
-
-    # Check if all boxes have been visited
-    return all(visited)
+    for key in keys:
+        if key < n and not opened[key]:
+            opened[key] = True
+            keys += boxes[key]
+    return all(opened)
 
